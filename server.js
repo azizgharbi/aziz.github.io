8b77ffb178d784782bf0bd1b210adc2a165fbf41
aziz gharbi <agharbi@inovva.com>
2018-06-05 09:19:47 -0400

    404 routes

--- server.js
+++ server.js
@@ -3,5 +3,16 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+
+/** 
+ * redirect 404  
+ */
+
+app.get('*',function(req,res){
+    return res.redirect("/");
+})
+
+
 var port = process.env.PORT || 5000;
 app.listen(port);
