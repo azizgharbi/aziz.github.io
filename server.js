12eb5bc63bc84f5f5401a8b910872348235a7678
aziz gharbi <agharbi@inovva.com>
2018-06-05 13:35:34 -0400

    change port

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+const indexFile = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
+app.use(indexFile);
+
 app.use(history({
   disableDotRule: true,
   verbose: true
 }));
-app.use(staticFileMiddleware);
+
+app.use(indexFile);
 
 app.get('/', function (req, res) {
   res.render(path.join(__dirname + '/dist/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
 });
